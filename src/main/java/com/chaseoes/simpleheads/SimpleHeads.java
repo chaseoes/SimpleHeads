2a3ee00f550e39392fd98fefac5c7105db1ccfac
chaseoes <chase.oes@gmail.com>
2013-05-19 19:50:40 -0700

    Initial commit!

---
+++ src/main/java/com/chaseoes/simpleheads/SimpleHeads.java
@@ -0,0 +1,37 @@
+package com.chaseoes.simpleheads;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleHeads extends JavaPlugin {
+
+	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+		if (cs instanceof Player) {
+			if (strings.length == 1) {
+				((Player) cs).getInventory().addItem(getHead(strings[0]));
+				cs.sendMessage(ChatColor.DARK_AQUA + "There you go!");
+			} else {
+				cs.sendMessage(ChatColor.RED + "Usage: /head <player>");
+			}
+		} else {
+			cs.sendMessage("You must be a player to use that command.");
+		}
+		return true;
+	}
+
+	public ItemStack getHead(String player) {
+		ItemStack i = new ItemStack(Material.SKULL_ITEM);
+		i.setDurability((short) 3);
+		SkullMeta sm = (SkullMeta) i.getItemMeta();
+		sm.setOwner(player);
+		i.setItemMeta(sm);
+		return i;
+	}
+
+}
