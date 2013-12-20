9de806988c872e512621f889862d908bf9a764cf
chaseoes <chase.oes@gmail.com>
2013-12-20 16:38:28 -0700

    Add simpleheads.use permission.

--- src/main/java/com/chaseoes/simpleheads/SimpleHeads.java
+++ src/main/java/com/chaseoes/simpleheads/SimpleHeads.java
@@ -11,27 +11,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleHeads extends JavaPlugin {
 
-	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
-		if (cs instanceof Player) {
-			if (strings.length == 1) {
-				((Player) cs).getInventory().addItem(getHead(strings[0]));
-				cs.sendMessage(ChatColor.DARK_AQUA + "There you go!");
-			} else {
-				cs.sendMessage(ChatColor.RED + "Usage: /head <player>");
-			}
-		} else {
-			cs.sendMessage("You must be a player to use that command.");
-		}
-		return true;
-	}
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        if (cs instanceof Player) {
+            if (strings.length == 1) {
+                if (cs.hasPermission("simpleheads.use")) {
+                    ((Player) cs).getInventory().addItem(getHead(strings[0]));
+                    cs.sendMessage(ChatColor.DARK_AQUA + "There you go!");
+                } else {
+                    cs.sendMessage(ChatColor.DARK_AQUA + "You don't have permission.");
+                }
+            } else {
+                cs.sendMessage(ChatColor.RED + "Usage: /head <player>");
+            }
+        } else {
+            cs.sendMessage("You must be a player to use that command.");
+        }
+        return true;
+    }
 
-	public ItemStack getHead(String player) {
-		ItemStack i = new ItemStack(Material.SKULL_ITEM);
-		i.setDurability((short) 3);
-		SkullMeta sm = (SkullMeta) i.getItemMeta();
-		sm.setOwner(player);
-		i.setItemMeta(sm);
-		return i;
-	}
+    public ItemStack getHead(String player) {
+        ItemStack i = new ItemStack(Material.SKULL_ITEM);
+        i.setDurability((short) 3);
+        SkullMeta sm = (SkullMeta) i.getItemMeta();
+        sm.setOwner(player);
+        i.setItemMeta(sm);
+        return i;
+    }
 
 }
